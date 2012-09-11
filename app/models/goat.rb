05335cd61240a77fba6ba24a4c02752a13f1ba3c
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-11 00:22:43 -0500

    Removed goat model after confirmed that it works on heroku

--- app/models/goat.rb
+++
@@ -1,3 +0,0 @@
-class Goat < ActiveRecord::Base
-  attr_accessible :name
-end
--- db/migrate/20120911051451_create_goats.rb
+++
@@ -1,9 +0,0 @@
-class CreateGoats < ActiveRecord::Migration
-  def change
-    create_table :goats do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
