949116e10d2d7ce8dcdbda07d3c4640050fa7d59
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-10 23:08:58 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RnaSeqAnalysisPipeline::Application.config.secret_token = 'e31c0f2c204c676cbc70d81301a5016fdda6d0bc25be5546be4c04fb740b212e39e98d05bbfcb0fc5e6cd758018f49477bcab9b7facb61b8f3f777a34e08bccc'
