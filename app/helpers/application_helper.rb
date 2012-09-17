1572b087834c8d7a8e31626938d78e7b5735fa40
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-16 23:04:58 -0500

    Added some css and graphics to the prototype.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    
+    def logo
+        logo = image_tag("powercat-glowing-android.jpg", :alt => "RNA-Seq Analysis pipeline", :class => "logo")
+    end
+    def busy_indicator
+        logo = image_tag("rna.gif", :alt => "RNA-Seq Analysis pipeline", :class => "logo")
+    end
 end
