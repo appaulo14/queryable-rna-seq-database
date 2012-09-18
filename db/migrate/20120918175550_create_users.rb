ad8452c51d926bacd2830a4719066498583003c2
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-18 13:29:43 -0500

    Did some planning to add a user and job models to aid in processing analysis

---
+++ db/migrate/20120918175550_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users, :primary_key => 'eID') do |t|
+      t.string :eID
+      t.string :email, :null => false
+
+      t.timestamps
+    end
+    add_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120918175550) do
+
+  create_table "users", :primary_key => "eID", :force => true do |t|
+    t.string   "email",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
