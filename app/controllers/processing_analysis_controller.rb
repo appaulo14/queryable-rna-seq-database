c1491c5f469272b4e351aa42cc03e172f66fcd92
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-17 22:15:14 -0500

    Set up the main menu for processing analysis to route the user to different places based on their choices, but there are some bugs in it.

--- app/controllers/processing_analysis_controller.rb
+++ app/controllers/processing_analysis_controller.rb
@@ -1,22 +1,59 @@
 class ProcessingAnalysisController < ApplicationController
   
+  def initialize
+    puts "INITIALIZING"
+    super
+  end
+    
   def main_menu
       #Do we need the main_menu form?
+      @choices = []
+      @form_target = '/processing_analysis/main_menu'
+      @submit_text = 'Next'
+      @goat = 1
+      @step
        if request.get?
-          @@step = 1
+          @step = 1
           @title = 'What type of processing analysis would you like to do?'
-          @form_target = '/processing_analysis/main_menu'
-          @submit_text = 'Next'
-          @choices=[]
           @choices.push({:id=>"differential_expression",:text=>"Differential expression testing"})
           @choices.push({:id=>"novel_transcript_isoforms",:text=>"Discover novel transcript isoforms"})
+          @choices.push({:id=>"both",:text=>"Both of them"})
           render 'shared/radio_choices'
        else request.post?
            debugger if ENV['RAILS_DEBUG'] == "true"
-           if @@step == 1
-             redirect_to('http://www.microsoft.com')
+           if @step == 1
+             @step=2
+             case params[:choice]
+             when "differential_expression"
+                @title = 'Do you want to use a a reference genome for differential expression?'
+                @choices.push({:id=>"reference",:text=>"Use reference genome"})
+                @choices.push({:id=>"de_novo",:text=>"Don't use reference genome (de-novo)"})
+                render 'shared/radio_choices'
+             when "novel_transcript_isoforms"
+                redirect_to '/processing_analysis/reference_analysis_isoforms_only'
+             when "both"
+                 redirect_to '/processing_analysis/reference_analysis'
+             end
+           elsif @@step == 2
+               @@step=3
+               case params[:choice]
+               when "reference"
+                   redirect_to '/processing_analysis/reference_analysis'
+               else
+                   @title = 'Would you like to use cuffdiff or edgeR for differential expression?'
+                   @choices.push({:id=>"cuffdiff",:text=>"Cuffdiff"})
+                   @choices.push({:id=>"edgeR",:text=>"edgeR"})
+                   render 'shared/radio_choices'
+               end
+           elsif @@step == 3
+              @@step = 4
+              case params[:choice]
+              when "cuffdiff"
+                  redirect_to '/processing_analysis/de_novo_analysis_cuffdiff'
+              when "edgeR"
+                  redirect_to '/processing_analysis/de_novo_analysis_edgeR'
+              end
            end
-           #render 'main_menu_step_2'
        end
   end
 
