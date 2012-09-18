a3fb45594e943726788047ab58a94eebe780bb82
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-17 23:34:14 -0500

    Fixed the problems with the user select which processing analysis they want do.

--- app/controllers/processing_analysis_controller.rb
+++ app/controllers/processing_analysis_controller.rb
@@ -10,19 +10,10 @@ class ProcessingAnalysisController < ApplicationController
       @choices = []
       @form_target = '/processing_analysis/main_menu'
       @submit_text = 'Next'
-      @goat = 1
-      @step
-       if request.get?
-          @step = 1
-          @title = 'What type of processing analysis would you like to do?'
-          @choices.push({:id=>"differential_expression",:text=>"Differential expression testing"})
-          @choices.push({:id=>"novel_transcript_isoforms",:text=>"Discover novel transcript isoforms"})
-          @choices.push({:id=>"both",:text=>"Both of them"})
-          render 'shared/radio_choices'
-       else request.post?
+      if request.post?
            debugger if ENV['RAILS_DEBUG'] == "true"
-           if @step == 1
-             @step=2
+           if session[:step] == 1
+             session[:step] = 2
              case params[:choice]
              when "differential_expression"
                 @title = 'Do you want to use a a reference genome for differential expression?'
@@ -34,8 +25,8 @@ class ProcessingAnalysisController < ApplicationController
              when "both"
                  redirect_to '/processing_analysis/reference_analysis'
              end
-           elsif @@step == 2
-               @@step=3
+           elsif session[:step] == 2
+               session[:step]=3
                case params[:choice]
                when "reference"
                    redirect_to '/processing_analysis/reference_analysis'
@@ -45,8 +36,8 @@ class ProcessingAnalysisController < ApplicationController
                    @choices.push({:id=>"edgeR",:text=>"edgeR"})
                    render 'shared/radio_choices'
                end
-           elsif @@step == 3
-              @@step = 4
+           elsif session[:step] == 3
+              session[:step] = 4
               case params[:choice]
               when "cuffdiff"
                   redirect_to '/processing_analysis/de_novo_analysis_cuffdiff'
@@ -54,13 +45,35 @@ class ProcessingAnalysisController < ApplicationController
                   redirect_to '/processing_analysis/de_novo_analysis_edgeR'
               end
            end
-       end
+      else      #if not request.post?
+         session[:step] = 1
+         @title = 'What type of processing analysis would you like to do?'
+         @choices.push({:id=>"differential_expression",:text=>"Differential expression testing"})
+         @choices.push({:id=>"novel_transcript_isoforms",:text=>"Discover novel transcript isoforms"})
+         @choices.push({:id=>"both",:text=>"Both of them"})
+         render 'shared/radio_choices'
+      end
   end
 
   def quality_filtering
   end
 
   def reference_analysis
+      redirect_to '/processing_analysis/de_novo_analysis_cuffdiff'
+      sleep 5
+      redirect_to '/processing_analysis/de_novo_analysis_edgeR'
+#       if request.post?
+#         case session[:reference_analysis_step]
+#         when 'tophat'
+#             render 'tophat'
+#         when 'cufflinks'
+#             render 'cufflinks'
+#         when 'cuffcompare'
+#             render 'cuffcompare'
+#         when 'cuffdiff'
+#             render 'cuffdiff'
+#         end
+#       end
   end
 
   def reference_analysis_isoforms_only
