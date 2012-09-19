46987d22c454935e4523a67392677d3286a5cc06
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-09-18 19:57:14 -0500

    Sucessfully created the model and migration script for the user table.

--- db/migrate/20120918175550_create_users.rb
+++ db/migrate/20120918175550_create_users.rb
@@ -1,11 +1,19 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table(:users, :primary_key => 'eID') do |t|
-      t.string :eID
-      t.string :email, :null => false
+    #NOTE: I used the up and down methods instead of just the change method 
+    #   because using only the change method resulted in an irrevisible 
+    #   migation exception
+  
+  def up
+    create_table(:users, :primary_key => "eID") do |t|
+      t.string :email, :limit => 45, :null => false
 
       t.timestamps
     end
+    change_column :users, :eID, :string, :limit => 45
     add_index :users, :email, :unique => true
+  end
+  
+  def down
+      drop_table :users
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  eID        :string(45)       not null, primary key
+#  email      :string(45)       not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe User do
