8e68670a14011d467c32b1dc853aa97d355c839f
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-10-14 13:40:40 -0500

    Added all the arguments and options to the cufflinks view model.

--- app/view_models/processing_analysis/cufflinks_execution.rb
+++ app/view_models/processing_analysis/cufflinks_execution.rb
@@ -6,7 +6,22 @@ module Processing_Analysis
             #Identifier for rails
             :sample_id,
             #Arugments
-            
+            :aligned_reads,
+            #General options
+            :GTF, :GTF_guide, :mask_file, :frag_bias_correct,
+            :multi_read_correct, :library_type,
+            #Advanced Abundance Estimation Options
+            :frag_len_mean, :frag_len_std_dev, :upper_quartile_norm,
+            :total_hits_norm, :compatible_hits_norm, :max_mle_iterations,
+            :max_bundle_frags, :no_effective_length_correction,
+            #Advanced Assembly Options
+            :min_isoform_fraction, :pre_mrna_fraction, :max_intron_length,
+            :junc_alpha, :small_anchor_fraction, :min_frags_per_transfrag,
+            :overhang_tolerance, :max_bundle_length, :min_intron_length,
+            :trim_3_avgcov_thresh, :trim_3_dropoff_frac, :max_multiread_fraction,
+            :overlap_radius,
+            #Advanced Reference Annotation Based Transcript (RABT) Assembly Options
+            :three_overhang_tolerance, :intron_overhang_tolerance, :no_faux_reads
            
     
         def initialize(attributes = {})
