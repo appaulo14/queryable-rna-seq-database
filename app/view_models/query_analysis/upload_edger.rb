e8f8b1149f3b793633c0281278c505ade91895d3
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-11-19 01:31:00 -0600

    Did a little work on parsing files from Trinity, not just Trinity.fasta.

--- app/view_models/query_analysis/upload_edger.rb
+++ app/view_models/query_analysis/upload_edger.rb
@@ -3,7 +3,11 @@ class Upload_EdgeR
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :trinity_fasta_file, :differential_expression_file
+  attr_accessor :trinity_fasta_file, 
+                :gene_differential_expression_file, 
+                :transcript_differential_expression_file, 
+                :gene_fpkm_file, 
+                :transcript_fpkm_file
   
   validate :validate_trinity_fasta_file, :presence => true
   validate :validate_differential_expression_file, :presence => true
@@ -21,23 +25,27 @@ class Upload_EdgeR
     x = 1
     #Create new job
     job = Job.new
+    #Read the normalized transcripts FPKM file, writing transcripts and fpkm_samples
+    #Read the normalized genes FPKM file, writing to genes and fpkm_samples and connecting genes to transcripts
+    #Read the differential expression file for transcripts, writing the differential expression table
+    #Read the differential expression file for genes, writing the differential expression table
+    #Read the Trinity.fasta file, writing to the transcript table
     #Write Trinity.fasta to database
-    while not trinity_fasta_file.tempfile.eof?
-      line = trinity_fasta_file.readline
-      #If this is a fasta description line
-      if line.match(/\A>/)
-        transcript = Transcript.new(:job => job)
-        #transcript.name_from_program = 
-        #transcript.fasta_description = 
-        while not line.match(/\A>/)
-          fasta_sequence+=line
-        end
-        transcript.fasta_sequence = sequence
-      end
-    end
-    
-    #Delete Trinity.fasta file
+#     line = trinity_fasta_file.readline
+#     while not trinity_fasta_file.tempfile.eof?
+#       #If this is a fasta description line
+#       if line.match(/\A>/)
+#         transcript = Transcript.new(:job => job)
+#         #transcript.name_from_program = 
+#         #transcript.fasta_description = 
+#         while not line.match(/\A>/)
+#           fasta_sequence+=line
+#         end
+#         transcript.fasta_sequence = sequence
+#       end
+#     end
     #Write differential expression tests?
+    #Delete the files
   end
   
   def persisted?
