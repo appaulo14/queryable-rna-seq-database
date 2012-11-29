4dbe799f13f059a2ebf7f14c036e660f6f19a5f8
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-11-29 08:06:58 -0600

    Did more work adding tooltips to my Trinity with edgeR for genes and transcripts upload page.

--- app/views/query_analysis/upload_trinity_with_edger_genes_and_transcripts.js
+++ app/views/query_analysis/upload_trinity_with_edger_genes_and_transcripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function()
 {
-    var text = 'run_edgeR.pl produces an FPKM tracking file. ' +
+    //Declare variables
+    var elements = [];
+    var titles = [];
+    var texts = [];
+    //Tooltip for Trinity fasta file
+    elements[0] = 'trinity_fasta_tooltip';
+    titles[0] = 'Trinity.fasta file';
+    texts[0] = 'Trinity.pl produces a fasta file called Trinity.fasta ' +
+      'which contains the assembled transcripts. It is located in your ' +
+      'Trinity output directory.';
+    //Tooltip for Differential expression files
+    elements[1] = 'differential_expression_tooltip';
+    titles[1] = 'Differential Expression File(s)';
+    texts[1] = 'run_edgeR.pl produces an FPKM tracking file. ' +
       'It is the file in your edgeR output folder that ends in ".FPKM".'
-    $('#fpkm_tooltip')
+    //Tooltip for FPKM files
+    elements[2] = 'fpkm_tooltip';
+    titles[2] = 'FPKM Tracking File(s)';
+    texts[2] = 'run_edgeR.pl produces an FPKM tracking file. ' +
+      'It is the file in your edgeR output folder that ends in ".FPKM".'  
+      
+    for (var i = 0; i < elements.length; i++){
+      $('#' + elements[i])
             .removeData('qtip') 
             .qtip({
                     content: {
-                            text: text, 
+                            text: texts[i], 
                             title: {
-                                    text: "FPKM Tracking Files",
+                                    text: titles[i],
                                     button: true
                             }
                     },
@@ -25,4 +45,5 @@ $(document).ready(function()
                             classes: 'qtip-shadow qtip-' + 'dark'
                     }
             });
+    }
 }); 
