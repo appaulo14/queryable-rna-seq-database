e869c30bd42b34cae1586d56f73e7ca3fd7a66cb
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-12-12 18:31:16 -0600

    Got the sort arrows on my table to work so now users can click the table headers to change the sorting and the arrows will how which columns the table is being sorted by. This is still for the query for differentially expressed transcripts page.

--- app/helpers/query_analysis_helper.rb
+++ app/helpers/query_analysis_helper.rb
@@ -1,5 +1,13 @@
 module QueryAnalysisHelper
-    def self.goat
-        puts "GOAT"
-    end
+  def ascending_sort_arrow
+    return image_tag('ascending_sort_arrow.png', :class => 'sort_arrow')
+  end
+  
+  def descending_sort_arrow
+    return image_tag('descending_sort_arrow.png', :class => 'sort_arrow')
+  end
+  
+  def neutral_sort_arrow
+    return image_tag('neutral_sort_arrow.png', :class => 'sort_arrow')
+  end
 end
