3c78e43d04a9427a3f16fde8484d08e8271e42fe
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-12-15 15:46:18 -0600

    Did more work implementing Downloadify on the query for differentially expressed transcripts page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     
     def logo
-        logo = image_tag("powercat-glowing-android.jpg", :alt => "RNA-Seq Analysis pipeline", :class => "logo")
+        logo = image_tag("ksu/powercat-glowing-android.jpg", :alt => "RNA-Seq Analysis pipeline", :class => "logo")
     end
     def busy_indicator
         busy_indicator = image_tag("rna.gif", :alt => "RNA-Seq Analysis pipeline", :class => "busy_indicator")
