2d7fa3ddb309f570f101867883d8158708ffed2b
fatPerlHacker <fat.perl.hacker@gmail.com>
2012-12-15 13:19:42 -0600

    Reorganized my third party javascript libraries into the vendor directory of rails and started implementing downloadify on the query for differentially expresed transcripts page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.qtip
-//= require stupidtable.min
+//= require qtip/jquery.qtip
+//= require Stupid-Table-Plugin/stupidtable.min
 //= require_tree .
