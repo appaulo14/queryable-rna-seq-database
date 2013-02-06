52b2877e4ddd4ae98d1da4eb8d2ced3b457ee653
fatPerlHacker <fat.perl.hacker@gmail.com>
2013-02-05 18:59:21 -0600

    While I was writing a test for my table sorting, I found a bug. I have been working on fixing it but haven't yet been successful.

--- spec/features/query_analysis/query_transcript_isoforms_spec.rb
+++ spec/features/query_analysis/query_transcript_isoforms_spec.rb
@@ -16,120 +16,135 @@ describe 'Query Transcript Isoforms page' do
   
   it 'should display a failure message if any of the parameters are invalid'
   
-  #TODO
-#   it 'should have all the columns in the query results table ' +
-#      'be sortable', :js => true do
-#     visit 'query_analysis/query_transcript_isoforms'
-#     find_button('submit_query').click
-#     ths = all('#query_results_table thead tr th')
-#     ths[3].click
-#     all('#query_results_table tbody tr')[0].all('td')[3].text.should eq('-')
-#     #save_and_open_page
-#   end
+  it 'should disable the query button once it has been clicked'
   
-  it 'should provide links to the Gene Ontology website for each GO term',
-      :js => true do
+  #TODO
+  it 'should have all the columns in the query results table be sortable', 
+     :js => true do
     #Go to page and submit the query
     visit 'query_analysis/query_transcript_isoforms'
     find_button('submit_query').click
-    #Store the dataset and fpkm sample for later use
-    dataset_id = find('#query_transcript_isoforms_dataset_id').value
-    dataset = Dataset.find_by_id(dataset_id)
-    #Verify all the rows in the query results table by looping through them
-    all_rows = all('#query_results_table tbody tr')
-    all_rows.count.should eq(dataset.transcripts.count)
-    original_window_handle = page.driver.browser.window_handles.first
-    (0..all_rows.count-1).each do |n|
-      #Extract some variables to use below
-      tr = all_rows[n]
-      tds = tr.all('td')
-      transcript = dataset.transcripts[n]
-      #Verify the GO terms and their links for the transcript
-      if transcript.go_terms.empty?
-        tds[2].text.should eq('No GO ids found.')
-      else
-        #Verify all the go terms by looping through them
-        go_items = tds[2].all('li')
-        (0..go_items.count-1).each do |i|
-          #Extract some variables
-          html_go_item = go_items[i].text
-          go_id = transcript.go_terms[i].id
-          required_href = "http://amigo.geneontology.org/" +
-                          "cgi-bin/amigo/term_details?" +
-                          "term=#{go_id}"
-          #Verify the link href is correct
-          go_link = go_items[i].find('a')
-          go_link[:href].should eq(required_href)
-          #Verify the link goes to the right place by clicking it and examining
-          #     the page's contents
-          go_link.click
-          go_link_window_handle = page.driver.browser.window_handles.last
-          page.driver.browser.switch_to.window(go_link_window_handle)
-          expected_page_title = "AmiGO: Term Details for #{go_id}"
-          page.find('title').text.should eq(expected_page_title)
-          page.driver.browser.switch_to.window(original_window_handle)
+    ths = all('#query_results_table thead tr th')
+    trs = all('#query_results_table tbody tr')
+    (3..ths.count-1).each do |th_i|
+      #Sort ascending
+      ths[th_i].click
+      sorted_trs = trs.sort{|a,b| a.all('td')[th_i].text <=> b.all('td')[th_i].text}
+      html_trs = all('#query_results_table tbody tr')
+      (0..sorted_trs.count-1).each do |tr_i|
+        (3..sorted_trs[tr_i].all('td').count-1).each do |td_i|
+          #debugger
+          html_trs[tr_i].all('td')[td_i].text.should eq(sorted_trs[tr_i].all('td')[td_i].text)
         end
       end
+#       #Sort descending
+#       ths[i].click
     end
   end
   
-  it 'should provide links to each transcript fasta sequence' do
-    #Go to page and submit the query
-    visit 'query_analysis/query_transcript_isoforms'
-    find_button('submit_query').click
-    #Store the dataset id for later use
-    dataset_id = find('#query_transcript_isoforms_dataset_id').value
-    #Verify all the rows in the query results table by looping through them
-    (all('#query_results_table tbody tr')).each do |tr|
-      #Extract some variables to use below
-      tds = tr.all('td')
-      transcript = Transcript.where(:dataset_id => dataset_id,
-                                    :name_from_program => tds[0].text)[0]
-      #Verify the transcript's link looks correct
-      transcript_href = URI.escape("get_transcript_fasta?" +
-                        "dataset_id=#{dataset_id}" +
-                        "&transcript_name=#{tds[0].text}")
-      tds[0].should have_link(tds[0].text, :href => transcript_href)
-      #Verify the link goes to a page that at least looks kind of like the 
-      #    transcript's fasta
-      tds[0].find('a').click
-      transcript_fasta_regex = 
-          /\A>#{Regexp.escape(transcript.blast_seq_id)}.+\n([atcg]+\n)+\z/i
-      page.html.should match(transcript_fasta_regex)
-    end
-  end
+#   it 'should provide links to the Gene Ontology website for each GO term',
+#       :js => true do
+#     #Go to page and submit the query
+#     visit 'query_analysis/query_transcript_isoforms'
+#     find_button('submit_query').click
+#     #Store the dataset and fpkm sample for later use
+#     dataset_id = find('#query_transcript_isoforms_dataset_id').value
+#     dataset = Dataset.find_by_id(dataset_id)
+#     #Verify all the rows in the query results table by looping through them
+#     all_rows = all('#query_results_table tbody tr')
+#     all_rows.count.should eq(dataset.transcripts.count)
+#     original_window_handle = page.driver.browser.window_handles.first
+#     (0..all_rows.count-1).each do |n|
+#       #Extract some variables to use below
+#       tr = all_rows[n]
+#       tds = tr.all('td')
+#       transcript = dataset.transcripts[n]
+#       #Verify the GO terms and their links for the transcript
+#       if transcript.go_terms.empty?
+#         tds[2].text.should eq('No GO ids found.')
+#       else
+#         #Verify all the go terms by looping through them
+#         go_items = tds[2].all('li')
+#         (0..go_items.count-1).each do |i|
+#           #Extract some variables
+#           html_go_item = go_items[i].text
+#           go_id = transcript.go_terms[i].id
+#           required_href = "http://amigo.geneontology.org/" +
+#                           "cgi-bin/amigo/term_details?" +
+#                           "term=#{go_id}"
+#           #Verify the link href is correct
+#           go_link = go_items[i].find('a')
+#           go_link[:href].should eq(required_href)
+#           #Verify the link goes to the right place by clicking it and examining
+#           #     the page's contents
+#           go_link.click
+#           go_link_window_handle = page.driver.browser.window_handles.last
+#           page.driver.browser.switch_to.window(go_link_window_handle)
+#           expected_page_title = "AmiGO: Term Details for #{go_id}"
+#           page.find('title').text.should eq(expected_page_title)
+#           page.driver.browser.switch_to.window(original_window_handle)
+#         end
+#       end
+#     end
+#   end
   
-  it "should provide a link to the fastas for the gene's transcripts" do
-    #Go to page and submit the query
-    visit 'query_analysis/query_transcript_isoforms'
-    find_button('submit_query').click
-    #Store the dataset id for later use
-    dataset_id = find('#query_transcript_isoforms_dataset_id').value
-    #Verify all the rows in the query results table by looping through them
-    all('#query_results_table tbody tr').each do |tr|
-      #Extract some variables to use below
-      tds = tr.all('td')
-      gene = Gene.where(:dataset_id => dataset_id,
-                        :name_from_program => tds[1].text)[0]
-      #Verify the gene's link looks correct
-      gene_href = URI.escape("get_gene_fastas?dataset_id=#{dataset_id}" +
-          "&gene_name=#{tds[1].text}")
-      tds[1].should have_link(tds[1].text, :href => gene_href)
-      #Verify the link goes to a page that at least looks kind of like the 
-      #    the fastas for the gene's transcripts
-      tds[1].find('a').click
-      transcript_fasta_scan_regex = 
-          />.+\n(?:[atcg]+\n)+/i
-      transcript_fastas = page.html.scan(transcript_fasta_scan_regex)
-      transcript_fastas.count.should eq(gene.transcripts.count)
-      (0..gene.transcripts.count-1).each do |i|
-        transcript = gene.transcripts[i]
-        transcript_fasta_regex = 
-          /\A>#{Regexp.escape(transcript.blast_seq_id)}.+\n([atcg]+\n)+\z/i
-        transcript_fastas[i].should match(transcript_fasta_regex)
-      end
-    end
-  end
+#   it 'should provide links to each transcript fasta sequence' do
+#     #Go to page and submit the query
+#     visit 'query_analysis/query_transcript_isoforms'
+#     find_button('submit_query').click
+#     #Store the dataset id for later use
+#     dataset_id = find('#query_transcript_isoforms_dataset_id').value
+#     #Verify all the rows in the query results table by looping through them
+#     (all('#query_results_table tbody tr')).each do |tr|
+#       #Extract some variables to use below
+#       tds = tr.all('td')
+#       transcript = Transcript.where(:dataset_id => dataset_id,
+#                                     :name_from_program => tds[0].text)[0]
+#       #Verify the transcript's link looks correct
+#       transcript_href = URI.escape("get_transcript_fasta?" +
+#                         "dataset_id=#{dataset_id}" +
+#                         "&transcript_name=#{tds[0].text}")
+#       tds[0].should have_link(tds[0].text, :href => transcript_href)
+#       #Verify the link goes to a page that at least looks kind of like the 
+#       #    transcript's fasta
+#       tds[0].find('a').click
+#       transcript_fasta_regex = 
+#           /\A>#{Regexp.escape(transcript.blast_seq_id)}.+\n([atcg]+\n)+\z/i
+#       page.html.should match(transcript_fasta_regex)
+#     end
+#   end
+  
+#   it "should provide a link to the fastas for the gene's transcripts" do
+#     #Go to page and submit the query
+#     visit 'query_analysis/query_transcript_isoforms'
+#     find_button('submit_query').click
+#     #Store the dataset id for later use
+#     dataset_id = find('#query_transcript_isoforms_dataset_id').value
+#     #Verify all the rows in the query results table by looping through them
+#     all('#query_results_table tbody tr').each do |tr|
+#       #Extract some variables to use below
+#       tds = tr.all('td')
+#       gene = Gene.where(:dataset_id => dataset_id,
+#                         :name_from_program => tds[1].text)[0]
+#       #Verify the gene's link looks correct
+#       gene_href = URI.escape("get_gene_fastas?dataset_id=#{dataset_id}" +
+#           "&gene_name=#{tds[1].text}")
+#       tds[1].should have_link(tds[1].text, :href => gene_href)
+#       #Verify the link goes to a page that at least looks kind of like the 
+#       #    the fastas for the gene's transcripts
+#       tds[1].find('a').click
+#       transcript_fasta_scan_regex = 
+#           />.+\n(?:[atcg]+\n)+/i
+#       transcript_fastas = page.html.scan(transcript_fasta_scan_regex)
+#       transcript_fastas.count.should eq(gene.transcripts.count)
+#       (0..gene.transcripts.count-1).each do |i|
+#         transcript = gene.transcripts[i]
+#         transcript_fasta_regex = 
+#           /\A>#{Regexp.escape(transcript.blast_seq_id)}.+\n([atcg]+\n)+\z/i
+#         transcript_fastas[i].should match(transcript_fasta_regex)
+#       end
+#     end
+#   end
   
   #TODO
   it 'should allow the query results table to be downloadable as a text file'
@@ -142,35 +157,35 @@ describe 'Query Transcript Isoforms page' do
     
     #MAYBE TODO:Put the requirements that this test satisfies here??
     # TODO: Try all the samples and datasets
-    it 'should not filter when no filtering is selected', :js => true do
-      #Go to page 
-      visit 'query_analysis/query_transcript_isoforms'
-      #Extract dataset variables for later use
-      id_for_dataset_select_tag = 'query_transcript_isoforms_dataset_id'
-      dataset_select_tag = find_by_id(id_for_dataset_select_tag)
-      #Loop through all the datasets, checking each one
-      (0..dataset_select_tag.all('option').count-1).each do |dataset_index|
-        #Select the dataset
-        dataset_option = dataset_select_tag.all('option')[dataset_index].text
-        select(dataset_option, :from => id_for_dataset_select_tag)
-        #Store the dataset for later
-        dataset_id = dataset_select_tag.value
-        dataset = Dataset.find_by_id(dataset_id)
-        #Extract sample variables for later use
-        id_for_sample_select_tag = 'query_transcript_isoforms_sample_id'
-        sample_select_tag = find_by_id(id_for_sample_select_tag)
-        #Loop through all the samples, checking each one
-        (0..sample_select_tag.all('option').count-1).each do |sample_index|
-          #Select the sample
-          sample_option = sample_select_tag.all('option')[sample_index].text
-          select(sample_option, :from => id_for_sample_select_tag)
-          #Submit the query
-          find_button('submit_query').click
-          #Verify the results
-          verify_query_results_data(dataset, dataset.transcripts)
-        end
-      end
-    end
+#     it 'should not filter when no filtering is selected', :js => true do
+#       #Go to page 
+#       visit 'query_analysis/query_transcript_isoforms'
+#       #Extract dataset variables for later use
+#       id_for_dataset_select_tag = 'query_transcript_isoforms_dataset_id'
+#       dataset_select_tag = find_by_id(id_for_dataset_select_tag)
+#       #Loop through all the datasets, checking each one
+#       (0..dataset_select_tag.all('option').count-1).each do |dataset_index|
+#         #Select the dataset
+#         dataset_option = dataset_select_tag.all('option')[dataset_index].text
+#         select(dataset_option, :from => id_for_dataset_select_tag)
+#         #Store the dataset for later
+#         dataset_id = dataset_select_tag.value
+#         dataset = Dataset.find_by_id(dataset_id)
+#         #Extract sample variables for later use
+#         id_for_sample_select_tag = 'query_transcript_isoforms_sample_id'
+#         sample_select_tag = find_by_id(id_for_sample_select_tag)
+#         #Loop through all the samples, checking each one
+#         (0..sample_select_tag.all('option').count-1).each do |sample_index|
+#           #Select the sample
+#           sample_option = sample_select_tag.all('option')[sample_index].text
+#           select(sample_option, :from => id_for_sample_select_tag)
+#           #Submit the query
+#           find_button('submit_query').click
+#           #Verify the results
+#           verify_query_results_data(dataset, dataset.transcripts)
+#         end
+#       end
+#     end
     
     #TODO
     it 'should filter by class code ='
