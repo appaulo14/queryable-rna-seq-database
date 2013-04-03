a51f89d50bdc534bce5931ab0608e5b2e84141fe
Pawl <fat.perl.hacker@gmail.com>
2013-04-02 20:38:27 -0500

    Made some improvements to the user interface, mostly in the upload area of query analysis.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= requery jquery/jquery.min.js
+//= require jquery/jquery.min.js
 //= require qtip/jquery.qtip
 //= require jquery_ujs
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,9 @@ module ApplicationHelper
     def more_info_link(url)
       return link_to('More Information',url, :target => '_blank')
     end
+    
+    def more_info_link_to_wiki(path)
+      return link_to('More Information', "https://github.com/fatPerlHacker/" +
+                                    "queryable-rna-seq-database/wiki/#{path}")
+    end
 end
