166de329ad16e74bf8ae80f38d8b0b046eff01e6
Pawl <fat.perl.hacker@gmail.com>
2013-04-03 10:59:49 -0500

    Made some improvements to the views, mostly be adding help tips.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 RnaSeqAnalysisPipeline::Application.initialize!
 
+
+
 class Logger
   def format_message(level, time, progname, msg)
     "#{time.to_s(:db)} #{level} -- #{msg}\n"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 RnaSeqAnalysisPipeline::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   require 'debugger'
-
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
