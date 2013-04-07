15ab0e56de0237db483cb1d7c5973b13cf4bc3ba
fatPerlHacker <fat.perl.hacker@gmail.com>
2013-04-07 00:49:00 -0500

    Made some changes to allow the system to use blast2go on Beocat.

--- app/util/upload/go_term_finder_and_processor.rb
+++ app/util/upload/go_term_finder_and_processor.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'system_util'
 
 class GoTermFinderAndProcessor
   def initialize(uploaded_file,dataset)
@@ -35,16 +36,14 @@ class GoTermFinderAndProcessor
     Rails.logger.info "Running blastx for dataset: #{@dataset.id}"
     @blast_xml_output_file = Tempfile.new('blastx')
     @blast_xml_output_file.close
-    stdout, stderr, status = 
-      Open3.capture3("#{Rails.root}/bin/blast/bin/blastx " +
-                    "-remote -db nr " +
-                    "-query #{@uploaded_transcripts_file.tempfile.path} " +
-                    "-out #{@blast_xml_output_file.path} " +
-                    "-show_gis -outfmt '5' -evalue 1e-6")
-    #Raise an exception if there were errors with blastx
-    if not stderr.blank?
-      raise StandardError, stderr
-    end
+    SystemUtil.system!("#{Rails.root}/bin/blast2go/run_blastx_for_blast2go.pl " +
+                       "#{@uploaded_transcripts_file.tempfile.path} " +
+                       "#{@blast_xml_output_file.path}")
+#       Open3.capture3("#{Rails.root}/bin/blast/bin/blastx " +
+#                     "-remote -db nr " +
+#                     "-query #{@uploaded_transcripts_file.tempfile.path} " +
+#                     "-out #{@blast_xml_output_file.path} " +
+#                     "-show_gis -outfmt '5' -evalue 1e-6")
     Rails.logger.info "Finished blastx for dataset: #{@dataset.id}"
   end
   
@@ -53,17 +52,12 @@ class GoTermFinderAndProcessor
     blast2go_output_file = Tempfile.new('blast2go')
     blast2go_output_file.close
     blast2go_dir = "#{Rails.root}/bin/blast2go"
-    stdout, stderr, status = 
-      Open3.capture3("java -Xmx2000m " +
+    SystemUtil.system!("java -Xmx2000m " +
                       "-cp *:#{blast2go_dir}/ext/*:#{blast2go_dir}/* " +
                       "es.blast2go.prog.B2GAnnotPipe " +
                       "-in #{@blast_xml_output_file.path} " +
                       "-out #{blast2go_output_file.path} " +
                       "-prop #{blast2go_dir}/b2gPipe.properties -annot")
-    #Raise an exception if there were errors with blast2go
-    if not stderr.blank?
-      raise StandardError, stderr
-    end
     Rails.logger.info "Finished blast2go for dataset: #{@dataset.id}"
     #Delete the temporary files
     File.delete(@blast_xml_output_file.path)
